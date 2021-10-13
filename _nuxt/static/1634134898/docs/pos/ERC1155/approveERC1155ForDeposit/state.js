7f1d3af04e7d45d603e1dfbd5a005d342a139a65
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2021-10-13 14:25:47 +0000

    Deploying to gh-pages from @ maticnetwork/matic.js@3bbd9f5d5e729d0cf2c23caacef1e9b33d75c511 🚀

--- _nuxt/static/1634134898/docs/pos/ERC1155/approveERC1155ForDeposit/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1634134898",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fpos\u002FERC1155\u002FapproveERC1155ForDeposit",config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
