39a7f911ea53c31fa9e22ec1d2b9699eb84029a6
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2021-10-20 14:13:18 +0000

    Deploying to gh-pages from @ maticnetwork/matic.js@647d7ddbc69db39270387a0624a7680d43c371ec 🚀

--- _nuxt/static/1634735537/v3/docs/pos/erc721/withdraw-exit/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1634735537",layout:"default",error:a,serverRendered:true,routePath:"\u002Fv3\u002Fdocs\u002Fpos\u002Ferc721\u002Fwithdraw-exit",config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
