a28e82624809561d4a3b4bbd5619e113af3c9664
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2021-10-21 06:38:06 +0000

    Deploying to gh-pages from @ maticnetwork/matic.js@a7eeccd9041ad5a4f10504f5bfe3182ab6fc354e 🚀

--- _nuxt/static/1634741883/v3/docs/advanced/abi-manager/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1634741883",layout:"default",error:a,serverRendered:true,routePath:"\u002Fv3\u002Fdocs\u002Fadvanced\u002Fabi-manager",config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
