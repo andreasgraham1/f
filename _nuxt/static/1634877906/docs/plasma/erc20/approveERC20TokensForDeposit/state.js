764ceeb7b7a267557c95e2b165ec7ec6409942fe
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2021-11-09 03:00:51 +0000

    Deploying to gh-pages from @ maticnetwork/matic.js@b2f8f2c82e522e87c64d71c85aaad0ad8b2f01a7 🚀

--- _nuxt/static/1634877906/docs/plasma/erc20/approveERC20TokensForDeposit/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1634877906",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fplasma\u002Ferc20\u002FapproveERC20TokensForDeposit",config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1636426846/v3/docs/setup/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1636426846",layout:"default",error:a,serverRendered:true,routePath:"\u002Fv3\u002Fdocs\u002Fsetup",config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
