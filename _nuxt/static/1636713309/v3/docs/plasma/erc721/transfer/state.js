9e90cc468909c40ccc5e43d80a870048321f0a54
vidyaakrishna <vidyaakrishna@users.noreply.github.com>
2021-11-22 10:35:18 +0000

    Deploying to gh-pages from @ maticnetwork/matic.js@c9d2e23952ed342eb6726cd19a75426923a9a932 🚀

--- _nuxt/static/1636713309/v3/docs/plasma/erc721/transfer/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1636713309",layout:"default",error:a,serverRendered:true,routePath:"\u002Fv3\u002Fdocs\u002Fplasma\u002Ferc721\u002Ftransfer",config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
