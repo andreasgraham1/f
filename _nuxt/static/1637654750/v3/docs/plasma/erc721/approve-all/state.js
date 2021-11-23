afa3f2ef840ffcbdfaee52491d6850115f19e9c0
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2021-11-23 08:05:55 +0000

    Deploying to gh-pages from @ maticnetwork/matic.js@f0de2a2b7387f12fa84d7db444e633b657e1e7b9 🚀

---
+++ _nuxt/static/1637654750/v3/docs/plasma/erc721/approve-all/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1637654750",layout:"default",error:a,serverRendered:true,routePath:"\u002Fv3\u002Fdocs\u002Fplasma\u002Ferc721\u002Fapprove-all",config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
