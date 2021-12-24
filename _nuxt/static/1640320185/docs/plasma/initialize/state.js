6cfe5266a064c146eb557100bffee3cda3a70684
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2021-12-24 04:36:34 +0000

    Deploying to gh-pages from @ maticnetwork/matic.js@b7a710742a4549e8cc8ba05a8bc2b5b404b56c1d 🚀

--- _nuxt/static/1640320185/docs/plasma/initialize/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1640320185",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fdocs\u002Fplasma\u002Finitialize"));
