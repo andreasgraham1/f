b9853979901e9874adad047b3f927e0d34274ae8
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2022-03-10 11:55:08 +0000

    Deploying to gh-pages from @ maticnetwork/matic.js@3f244522114ec6002601945ce960ebaa603dab7a 🚀

---
+++ _nuxt/static/1646913303/docs/pos/erc1155/is-approved-all/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmatic.js\u002F_nuxt\u002Fstatic\u002F1646913303",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocs\u002Fpos\u002Ferc1155\u002Fis-approved-all",config:{_app:{basePath:"\u002Fmatic.js\u002F",assetsPath:"\u002Fmatic.js\u002F_nuxt\u002F",cdnURL:a}}}}(null));
